f0543b61715b768690b04ac9433e0f49a5ae3b76
Simon Sablowski <simonsablowski@users.noreply.github.com>
2016-12-12 12:10:50 +0100

    changing default paths

--- configuration.php
+++ configuration.php
@@ -6,15 +6,15 @@ $configuration['pathApplication'] = dirname(__FILE__) . '/';
 
 $configuration['includeDirectories'] = array(
 	$configuration['pathApplication'],
-	'D:/Entwicklung/api/',
-	'D:/Entwicklung/nacho/'
+	$configuration['pathApplication'] . '../motivado-api/',
+	$configuration['pathApplication'] . '../nacho/'
 );
 
 $configuration['Database'] = array(
 	'type' => 'MySql',
 	'host' => 'localhost',
 	'name' => 'motivado_ui',
-	'user' => 'root',
+	'user' => 'motivado',
 	'password' => ''
 );
 
